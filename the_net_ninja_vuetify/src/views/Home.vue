29c0c0876d6469d734d824c2cd038b113516a047
fahico98 <fahico98@gmail.com>
2020-05-01 23:52:23 -0500

    Commit_3

--- the_net_ninja_vuetify/src/views/Home.vue
+++ the_net_ninja_vuetify/src/views/Home.vue
@@ -1,18 +1,23 @@
+
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+   <div class="home">
+      <h2>Homepage...!</h2>
+      <p class="red lighten-4 red--text text--darken-3">
+         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum sit nam reiciendis adipisci voluptatum, 
+         velit ducimus odit sapiente provident veniam, magni consequatur aliquid, quaerat quod temporibus facere. 
+         Accusantium, dignissimos fugit.
+      </p>
+      <h1 class="deep-orange--text text--accent-3 display-4 font-weight-bold">Massive bold display</h1>
+      <h1 class="deep-orange--text text--accent-3 caption font-weight-bold">Tiny bold display</h1>
+      <h1 class="deep-orange--text text--accent-3 display-4">Massive display</h1>
+      <h1 class="deep-orange--text text--accent-3 caption">Tiny display</h1>
+      <h1 class="deep-orange--text text--accent-3 display-4 font-weight-thin">Massive thin display</h1>
+      <h1 class="deep-orange--text text--accent-3 caption font-weight-thin">Tiny thin display</h1>
+   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+   export default {
+      name: 'Home'
+   }
 </script>
