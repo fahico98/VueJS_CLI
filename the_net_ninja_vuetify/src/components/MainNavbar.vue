efabab4e5ec98b92ecd7acea007dfab94dfa96a2
fahico98 <fahico98@gmail.com>
2020-05-03 11:21:21 -0500

    Commit_5

---
+++ the_net_ninja_vuetify/src/components/MainNavbar.vue
@@ -0,0 +1,24 @@
+
+<template>
+   
+   <v-toolbar flat>
+      <v-toolbar-title class="blue-grey--text">
+         <span class="text-uppercase font-weight-bold">Todo</span>
+         <span class="text-capitalize font-weight-light">Fahico</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn depressed text class="text-capitalize blue-grey--text">
+         <span>Sign Out</span>
+         <v-icon right>mdi-exit-to-app</v-icon>
+      </v-btn>
+   </v-toolbar>
+
+</template>
+
+<script>
+
+   export default {
+      name: "main-navbar"
+   }
+
+</script>
